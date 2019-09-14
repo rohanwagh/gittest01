97f6b9206802ebd2f830bc5afccbd8a2447a0670
Rohan Wagh <rohan.vjti@gmail.com>
2019-09-13 18:29:45 -0700

    Third commit

--- rohan/src/test/java/com/rohan/gittest/GitTestDemo01.java
+++ rohan/src/test/java/com/rohan/gittest/GitTestDemo01.java
@@ -5,7 +5,7 @@ public class GitTestDemo01
 	public static void main(String[] args)
 	{
 		System.out.println("Git Test");
-		System.out.println("Second Committ");
+		System.out.println("Second Commit");
 	}
 
 }
